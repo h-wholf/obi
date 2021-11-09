78ca66851d87d9f816cda4f9e191387b37833226
joaquin osorio vazquez <kemalmoto.e5@gmail.com>
2021-11-08 23:08:37 -0600

    primer

---
+++ js/funciones.js
@@ -0,0 +1,55 @@
+function saludar() {
+    alert("hola mensaje de prueba");
+}
+
+function faucetpay() {
+
+    window.location.href = "https://faucetpay.io/?r=1170362";
+}
+
+function betfury() {
+
+    window.location.href = "https://betfury.io/?r=60b1766b1fd1625ac2ade16e"
+}
+
+function trustdice() {
+
+    window.location.href = "https://trustdice.win/faucet/?ref=u_sbp614"
+}
+
+function freebitco() {
+
+    window.location.href = "https://freebitco.in/?r=37948571"
+}
+
+function youtube() {
+
+    window.location.href = "https://www.youtube.com/channel/UCoTGS-OX0F_RGYMzTJ-_cAQ"
+}
+
+function whatsapp() {
+
+    window.location.href = "https://wa.me/522226320768";
+
+}
+
+function ecoin() {
+
+    window.location.href = "https://ecoinofficial.org/referral/d7gubs0";
+
+}
+
+function autofaucet() {
+
+    window.location.href = "https://autofaucet.org/r/Kemal21"
+}
+
+function feyorratop() {
+
+    window.location.href = "https://feyorra.top/?r=68742"
+}
+
+function luckyfish() {
+
+    window.location.href = "https://luckyfish.xyz/?c=c_kemal21"
+}
